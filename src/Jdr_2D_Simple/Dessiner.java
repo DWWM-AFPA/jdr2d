b3bf2cc4e9e993f58addb4dcbbf1ccdc844895ed
DamienSerrurier <damien.ser@laposte.net>
2023-02-05 23:09:46 +0100

    Ajout de la gestion des lieux dans le monde comme la position et la taille

--- src/Jdr_2D_Simple/Dessiner.java
+++ src/Jdr_2D_Simple/Dessiner.java
@@ -1,95 +1,104 @@
 package Jdr_2D_Simple;
 
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Dessiner implements Visitor{
+public class Dessiner implements Visitor {
 
     private Monde monde;
     private Lieu lieu;
-    private char[][] position;
     private char[][] dessiner;
-    private ArrayList<Object> objects;
+    private int xPosition;
+    private int yPosition;
+    private String stockageMonde;
 
-    public Monde getMonde(){
+    //private ArrayList<Lieu> lieux;
+
+    //private ArrayList<Object> objects;
+    public Monde getMonde() {
         return monde;
     }
 
-    public Dessiner setMonde(Monde monde){
+    public Dessiner setMonde(Monde monde) {
         this.monde = monde;
         return this;
     }
 
-    public Lieu getLieu(){
+    public Lieu getLieu() {
         return lieu;
     }
 
-    public Dessiner setLieu(Lieu lieu){
+    public Dessiner setLieu(Lieu lieu) {
         this.lieu = lieu;
         return this;
     }
 
-    public char[][] getPosition(){
-        return position;
+    public int getxPosition() {
+        return xPosition;
     }
 
-    public Dessiner setPosition(char[][] position){
-        this.position = position;
-        return this;
-    }
-    public char[][] getDessiner(){
-        return dessiner;
-    }
-
-    public Dessiner setDessiner(char[][] dessiner){
-        this.dessiner = dessiner;
+    public Dessiner setxPosition(int xPosition) {
+        this.xPosition = xPosition;
         return this;
     }
 
-    public ArrayList<Object> getObject(){
+    public int getyPosition() {
+        return xPosition;
+    }
+
+    public Dessiner setyPosition(int yPosition) {
+        this.yPosition = yPosition;
+        return this;
+    }
+   /* public ArrayList<Object> getObject(){
         return objects;
     }
 
     public Dessiner setObjects(ArrayList<Object> objects){
         this.objects = objects;
         return this;
-    }
-    public Dessiner(Monde monde){
-        this.monde = monde;
-        this.dessiner = generationMondes(getMonde().getxTaille(), getMonde().getyTaille());
-        this.lieu = rechercheLieu();
+    }*/
+
+    /*public String getStockageMonde() {
+        return stockageMonde;
     }
 
-    public Dessiner(Lieu lieu){
+    public Dessiner setStockageMonde(String stockageMonde) {
+        this.stockageMonde = stockageMonde;
+        return this;
+    }*/
+
+    /*public ArrayList<Lieu> getLieux(){
+        return lieux;
+    }
+
+    public Dessiner setLieux(ArrayList<Lieu> lieux){
+        this.lieux = lieux;
+        return this;
+    }*/
+
+    public Dessiner(Monde monde, Lieu lieu) {
+        this.monde = monde;
+        this.lieu = lieu;
+        this.dessiner = generationMondes(getMonde().getxTaille(), getMonde().getyTaille());
+    }
+
+    /*public Dessiner(Lieu lieu){
         this.lieu = lieu;
         this.dessiner = generationLieux(getLieu().getxTaille(), getLieu().getyTaille());
-    }
+    }*/
 
-    public Dessiner(Monde monde, Object o){
+    /*public Dessiner(Monde monde, Lieu lieu){
         this.monde = monde;
+        this.lieu = lieu;
         this.dessiner = generationMondes(getMonde().getxTaille(), getMonde().getyTaille());
-        this.lieu = rechercheLieu();
-        this.objects = new ArrayList<>();
-        this.add(o);
+        this.lieux = new ArrayList<>();
+        this.add(lieu);
     }
 
-    public Dessiner add(Object o){
-        this.objects.add(o);
+    public Dessiner add(Lieu l){
+        this.lieux.add(l);
         return this;
-    }
-
-    /*public char[][] generationPosition(int xPosition, int yPosition){
-
-        int i, j;
-        char[][] retour = new char[xPosition][yPosition];
-
-        for(i = 0; i < getMonde().getyPosition(); i++){
-            for(j = 0; j < getMonde().getxPosition(); j++){
-                retour[j][i] += ' ';
-            }
-        }
-        return retour;
     }*/
 
     public char[][] generationMondes(int xTaille, int yTaille){
@@ -99,9 +108,9 @@ public class Dessiner implements Visitor{
         int compteurX = getMonde().getxTaille() - 1;
         int compteurY = getMonde().getyTaille() - 1;
 
-        for(i = 0; i < getMonde().getyTaille(); i++){
-            for(j = 0; j < getMonde().getxTaille(); j++){
-                if(i == 0 || j == 0 || i == compteurY || j == compteurX){
+        for(i = 0; i < getMonde().getyTaille(); i++) {
+            for(j = 0; j < getMonde().getxTaille(); j++) {
+                if(i == 0 || j == 0 || i == compteurY || j == compteurX) {
                     retour[j][i] += '#';
                 }else{
                     retour[j][i] += ' ';
@@ -111,90 +120,133 @@ public class Dessiner implements Visitor{
         return retour;
     }
 
-    public char[][] generationLieux(int xTaille, int yTaille){
+    public char[][] generationLieux(int xTaille, int yTaille) {
 
         int i, j;
         Random choix = new Random();
         char[][] retour = new char[xTaille][yTaille];
 
-        for (i = 0; i < getLieu().getyTaille(); i++){
-            for (j = 0; j < getLieu().getxTaille(); j++){
-                if (choix.nextInt(100) + 1 < 95){
+        for (i = 0; i < getLieu().getyTaille(); i++) {
+            for (j = 0; j < getLieu().getxTaille(); j++) {
+                if (choix.nextInt(100) + 1 < 80) {
+
                     retour[j][i] += '.';
                 }else{
-                    retour[j][i] = '#';
+                    retour[j][i] = 'Y';
                 }
             }
         }
         return retour;
     }
-    public Lieu rechercheLieu(){
+    /*public void rechercheLieu(){
 
-        if(getMonde().getLieux() != null){
-            for(Lieu myLieu : getMonde().getLieux()){
-                lieu = myLieu;
+        if(monde.getLieux() != null){
+            for(Lieu myLieu : monde.getLieux()){
+                dessiner =  generationLieux(myLieu.getxTaille(), myLieu.getyTaille());
             }
         }
-        return lieu;
-    }
+    }*/
 
-    public Object visit(Monde m){
+    public String dessinerMonde() {
 
-        int i, j, k, l;
+        int i, j, px, py, compteurX, compteurY;
         String retour = new String();
 
-        for(l = 0; l < m.getyPosition(); l++) {
+        for (py = 0; py < getMonde().getyPosition(); py++) {
             retour += "\n";
         }
-        for (i = 0; i < m.getyTaille(); i++){
-            for(k = 0; k < m.getxPosition(); k++){
+        for (i = 0; i < getMonde().getyTaille(); i++){
+            for (px = 0; px < getMonde().getxPosition(); px++) {
                 retour += " ";
             }
-            for (j = 0; j < m.getxTaille(); j++){
+            for (j = 0; j < getMonde().getxTaille(); j++) {
                 retour += dessiner[j][i];
             }
             retour += "\n";
         }
+
+        //Stockage du monde dessiner
+        stockageMonde = retour;
+
+        //Parcourt des lieux stockés dans le monde
+        ArrayList<Lieu> arrayLieux = getMonde().getArrayLieux();
+        if (arrayLieux != null) {
+            for (Lieu myLieu : arrayLieux) {
+                dessiner = generationLieux(myLieu.getxTaille(), myLieu.getyTaille());
+            }
+            retour += visit(getLieu());
+        }
+
         retour += "\n";
-        dessiner = generationLieux(lieu.getxTaille(), lieu.getyTaille());
         return retour;
     }
 
-    public Object visit(Lieu l){
+    public Object visit(Monde m) {
+        return dessinerMonde();
+    }
 
-        int i, j;
-        String retour = new String();
+    public Object visit(Lieu l) {
 
-        for(i = 0; i < l.getyTaille(); i++){
-            for(j = 0; j < l.getxTaille(); j++){
-                retour += dessiner[j][i];
+        int a, b, i, j, positionLieu;
+        int d = 0;
+        StringBuilder retour = new StringBuilder();
+        String marqueurEcriture = "";
+        char lieu = ' ';
+        positionLieu = getLieu().getxPosition() + (getMonde().getxTaille() + 1) * getLieu().getyPosition();
+        marqueurEcriture += stockageMonde.charAt(positionLieu);
+
+        for (i = 0; i < getLieu().getyTaille(); i++) {
+            for (j = 0; j < getLieu().getxTaille(); j++) {
+                lieu =  dessiner[j][i];
             }
-            retour += "\n";
         }
-        return retour;
+
+        for (a = 0; a < stockageMonde.length(); a++) {
+
+            if(a == positionLieu) {
+                //Taille du lieu en Y
+                for (b = 0; b < getLieu().getyTaille(); b++) {
+                    //Taille du lieu en X avec remplacement des morceaux du monde par des morceaux du lieu
+                    for (a = positionLieu + d; a < getLieu().getxTaille() + positionLieu + d; a++) {
+                        retour.append(marqueurEcriture.replace(stockageMonde.charAt(a), lieu));
+                    }
+                    //Gestion de l'espace à chaque nouvelles lignes de lieu
+                    for (a = positionLieu + getLieu().getxTaille() + d; a < positionLieu + getMonde().getxTaille() + d; a++) {
+                        retour.append(stockageMonde.charAt(a));
+                    }
+                    retour.append(stockageMonde.charAt(a));
+                    d += getMonde().getxTaille() + 1;
+                }
+
+            } else {
+                retour.append(stockageMonde.charAt(a));
+            }
+        }
+
+        return retour.toString();
     }
 
-    public Object visit(Objet m){
+    public Object visit(Objet m) {
         return m;
     }
 
-    public Object visit(Arme m){
+    public Object visit(Arme m) {
         return m;
     }
 
-    public Object visit(Armure m){
+    public Object visit(Armure m) {
         return m;
     }
 
-    public Object visit(Bouclier m){
+    public Object visit(Bouclier m) {
         return m;
     }
 
-    public Object visit(Materiel m){
+    public Object visit(Materiel m) {
         return m;
     }
 
-    public Object visit(Personnage m){
+    public Object visit(Personnage m) {
         return m;
     }
 }
