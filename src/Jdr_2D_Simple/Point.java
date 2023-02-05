19eb17d3c1185f8d9cf24c11accbcb3a0c6c801c
DamienSerrurier <damien.ser@laposte.net>
2023-02-05 23:06:41 +0100

    Ajout de la classe Poin permettant la gestion de la position des mondes, lieux, Objets, etc

--- src/Jdr_2D_Simple/Point.java
+++ src/Jdr_2D_Simple/Point.java
@@ -1,2 +1,30 @@
-package Jdr_2D_Simple;public class Point {
+package Jdr_2D_Simple;
+
+public abstract class Point {
+
+    protected int xPosition;
+    protected int yPosition;
+
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public Point setxPosition(int xPosition) {
+        this.xPosition = xPosition;
+        return this;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public Point setyPosition(int yPosition) {
+        this.yPosition = yPosition;
+        return this;
+    }
+
+    public Point(int xPosition, int yPosition) {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+    }
 }
