1b8af26893e52e6e67b41339689ed77f7058ed4f
DamienSerrurier <damien.ser@laposte.net>
2023-02-05 23:04:41 +0100

    Modification de la gestion de la position

--- src/Jdr_2D_Simple/Objet.java
+++ src/Jdr_2D_Simple/Objet.java
@@ -1,6 +1,6 @@
 package Jdr_2D_Simple;
 
-public abstract class Objet extends Monde implements Visitable{
+public abstract class Objet extends Point implements Visitable {
     //Menmbres
     protected String nomObjet;
     protected String descriptionObjet;
@@ -10,83 +10,82 @@ public abstract class Objet extends Monde implements Visitable{
     protected int poidsObjet;
     protected boolean recompenseObjet;
     protected boolean queteObjet;
-
     protected Materiel materielObjet;
 
     //Getter et Setter
-    public String getNomObjet(){
+    public String getNomObjet() {
         return nomObjet;
     }
 
-    public Objet setNomObjet(String nomObjet){
+    public Objet setNomObjet(String nomObjet) {
         this.nomObjet = nomObjet;
         return this;
     }
 
-    public String getDescriptionObjet(){
+    public String getDescriptionObjet() {
         return descriptionObjet;
     }
 
-    public Objet setDescriptionObjet(String descriptionObjet){
+    public Objet setDescriptionObjet(String descriptionObjet) {
         this.descriptionObjet = descriptionObjet;
         return this;
     }
 
-    public int getPrixObjet(){
+    public int getPrixObjet() {
         return prixObjet;
     }
 
-    public Objet setPrixObjet(int prixObjet){
+    public Objet setPrixObjet(int prixObjet) {
         this.prixObjet = prixObjet;
         return this;
     }
 
-    public boolean getOuvertObjet(){
+    public boolean getOuvertObjet() {
         return ouvertObjet;
     }
 
-    public Objet setOuvertObjet(boolean ouvertObjet){
+    public Objet setOuvertObjet(boolean ouvertObjet) {
         this.ouvertObjet = ouvertObjet;
         return this;
     }
 
-    public int getQuantiteObjet(){
+    public int getQuantiteObjet() {
         return quantiteObjet;
     }
 
-    public Objet setQuantiteObjet(int quantiteObjet){
+    public Objet setQuantiteObjet(int quantiteObjet) {
         this.quantiteObjet = quantiteObjet;
         return this;
     }
 
-    public int getPoidsObjet(){
+    public int getPoidsObjet() {
         return poidsObjet;
     }
 
-    public Objet setPoidsObjet(int poidsObjet){
+    public Objet setPoidsObjet(int poidsObjet) {
         this.poidsObjet = poidsObjet;
         return this;
     }
 
-    public boolean getRecompenseObjet(){
+    public boolean getRecompenseObjet() {
         return recompenseObjet;
     }
 
-    public Objet setRecompenseObjet(boolean recompenseObjet){
+    public Objet setRecompenseObjet(boolean recompenseObjet) {
         this.recompenseObjet = recompenseObjet;
         return this;
     }
 
-    public boolean getQueteObjet(){
+    public boolean getQueteObjet() {
         return queteObjet;
     }
 
-    public Objet setQueteObjet(boolean queteObjet){
+    public Objet setQueteObjet(boolean queteObjet) {
         this.queteObjet = queteObjet;
         return this;
     }
 
-    public Materiel getMaterielObjet(){
+    public Materiel getMaterielObjet() {
         return materielObjet;
     }
 
@@ -96,6 +95,7 @@ public abstract class Objet extends Monde implements Visitable{
     }
 
     public Objet(String nomObjet, int prixObjet, int quantiteObjet, int poidsObjet, boolean recompenseObjet, boolean queteObjet, int xPosition, int yPosition) {
+        super(xPosition, yPosition);
         this.nomObjet = nomObjet;
         this.prixObjet = prixObjet;
         this.poidsObjet = poidsObjet;
@@ -104,7 +104,7 @@ public abstract class Objet extends Monde implements Visitable{
         this.quantiteObjet = quantiteObjet;
     }
 
-    public Object accept(Visitor v){
+    public Object accept(Visitor v) {
         return v.visit(this);
     }
 }
